d446a63fb7915f91cfc36078d950daae6d9f2087
Andrew Andrianov <andrew@ncrmnt.org>
2015-12-31 01:09:48 +0300

    Fix compilation problems against recent aura git
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- main.c
+++ main.c
@@ -8,9 +8,10 @@ int main() {
 	struct aura_buffer *retbuf; 
 
 	ret = aura_call(n, "echo_u16", &retbuf, 0x0102);
-	slog(0, SLOG_DEBUG, "call ret %d", ret);
+	if (ret != 0)
+		exit(1);
 	aura_hexdump("Out buffer", retbuf->data, retbuf->size);
-	aura_buffer_release(n, retbuf);
+	aura_buffer_release(retbuf);
 	aura_close(n);
 
 	return 0;
